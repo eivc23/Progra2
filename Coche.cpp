5f5cefea1a78eb4bd80dc0c0301ac8e1de3339b4
Edward <edward.vasquez@uniat.edu.mx>
2024-05-09 10:04:04 -0600

    On branch master  Initial commit

---
+++ Coche.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Alumnos on 12/03/2024.
+//
+
+#include "Coche.h"
---
+++ Coche.h
@@ -0,0 +1,14 @@
+//
+// Created by Alumnos on 12/03/2024.
+//
+
+#ifndef UNTITLED1_COCHE_H
+#define UNTITLED1_COCHE_H
+
+
+class Coche {
+
+};
+
+
+#endif //UNTITLED1_COCHE_H
